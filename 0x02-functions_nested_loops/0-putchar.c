fd52d27c61dcbc27db32671902545c42a354e676
Emmanuel0846 <olaoluwaidowu166@gmail.com>
2023-02-21 16:44:33 -0800

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,14 +8,15 @@
  * Return: Always 0 (success)
  */
 int main(void)
-{
-	char text[8] = "_putchar";
-	int i = 0;
-
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(text[i]);
-	}
+{	
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
