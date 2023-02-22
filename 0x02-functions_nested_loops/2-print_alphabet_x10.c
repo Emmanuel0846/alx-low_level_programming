40757b8c47754e6a71356497034b5cb38db05e50
Emmanuel0846 <olaoluwaidowu166@gmail.com>
2023-02-21 17:56:14 -0800

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+
+/**
+ * print_alphabet_x10 - entry point
+ *
+ * Description: function that prints 10 times the alphabet,
+ * in lowercase, followed by a new line
+ *
+ * Return: void
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+	while (i <= 9)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+
+		}
+		_putchar('\n');
+		i++;
+	}
+}
