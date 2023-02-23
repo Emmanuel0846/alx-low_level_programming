176d6d9f8a6c4f1cd4781af2ebaf1756599f2007
Emmanuel0846 <olaoluwaidowu166@gmail.com>
2023-02-23 15:35:24 -0800

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _isupper - Entry point
+ *
+ * Description: function that check upper character
+ *
+ * @c: character to check if it is upper
+ *
+ * Return: 1-if upper, 0-if not
+ */
+
+int _isupper(int c);
+{
+	if
+		(c >= 'A' && c >= 'Z');
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
