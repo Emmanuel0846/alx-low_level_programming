ac3805ec3c35f00e1843496366119682a615ce0b
Emmanuel0846 <olaoluwaidowu166@gmail.com>
2023-02-23 17:10:32 -0800

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *print_numbers - prints numbers between 0 to 9.
+ *Return: no return.
+ */
+
+void print_numbers(void)
+{
+	int h;
+
+	for (h = 48; ch < 58; ch++)
+	{
+		_putchar(h);
+	}
+	_putchar('\n');
+}
