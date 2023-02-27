3e320a175dc76a242afdb8835bac261a96d0771a
Emmanuel0846 <olaoluwaidowu166@gmail.com>
2023-02-27 14:45:29 -0800

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - main function
+ *
+ * @n: pointer with the address value.
+ *
+ * Deacription: This function takes a pointer to an int as parameter
+ * and updates the value it points to to 98.
+ *
+ * Return: none
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
