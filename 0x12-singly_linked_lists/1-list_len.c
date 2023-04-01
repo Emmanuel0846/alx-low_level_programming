28244c8f42c6c86c2660d86619c8ae11868bba27
Emmanuel0846 <olaoluwaidowu166@gmail.com>
2023-03-31 23:12:03 -0700

    singly linked list newly update

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * list_len - finds the number of nodes in a list
+ * project by Olaoluwa Emmanuel
+ * @h: singly linked list
+ * Return: number of elements in a linked list
+ */
+size_t list_len(const list_t *h)
+{
+	int j;
+
+	for (j = 0; h; i++)
+		h = h->next;
+	return (j);
+}
