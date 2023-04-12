03af9836af8693d442e516072a67436fb023a507
Emmanuel0846 <olaoluwaidowu166@gmail.com>
2023-04-11 19:14:19 -0700

    cp updated

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *createBuffer(char *myFile);
-void closeFile(int fileDesc);
+char *create_buffer(char *file);
+void close_file(int fd);
 
 /**
- * createBuffer -Entry point to allocate 1024 bytes for a buffer.
- * pROJECT BY Olaoluwa Emmanuel IDOWU
- * @myFile: The name of the file buffer is storing chars for.
+ * create_buffer - Entry point to allocate 1024 bytes for a buffer.
+ * Project by Olaoluwa Emmanuel IDOWU
+ * @file: The name of the file buffer is storing chars for.
  *
  * Return: A pointer to the newly-allocated buffer.
  */
-char *createBuffer(char *myFile)
+char *create_buffer(char *file)
 {
 	char *buff;
 
@@ -21,7 +21,7 @@ char *createBuffer(char *myFile)
 	if (buff == NULL)
 	{
 		dprintf(STDERR_FILENO,
-			"Error: Can't write to %s\n", myFile);
+			"Error: Can't write to %s\n", file);
 		exit(99);
 	}
 
@@ -29,30 +29,30 @@ char *createBuffer(char *myFile)
 }
 
 /**
- * closeFile -Entry point to close file descriptors.
- * @fileDesc: The file descriptor to be closed.
+ * close_file - Entry point to close file descriptions.
+ * @fd: The file descriptor to be closed.
  */
-void closeFile(int fileDesc)
+void close_file(int fd)
 {
-	int closeMe;
+	int m;
 
-	closeMe = close(fileDesc);
+	m = close(fd);
 
-	if (closeMe == -1)
+	if (m == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fileDesc %d\n", fileDesc);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 }
 
 /**
- * main - Entry point to  Copy the contents of a file to another file.
+ * main - Entry point to copy the contents of a file to another file.
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
  *
  * Return: 0 on success.
  *
- * Description: If the argument count is incorrect - exit code 97.
+ * Description: If the argument count  incorrect - exit code 97.
  *              If file_from does not exist or cannot be read - exit code 98.
  *              If file_to cannot be created or written to - exit code 99.
  *              If file_to or file_from cannot be closed - exit code 100.
@@ -60,7 +60,7 @@ void closeFile(int fileDesc)
 int main(int argc, char *argv[])
 {
 	int from, to, readMe, writeMe;
-	char *buff;
+	char *buffer;
 
 	if (argc != 3)
 	{
@@ -68,9 +68,9 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 
-	buff = createBuffer(argv[2]);
+	buffer = create_buffer(argv[2]);
 	from = open(argv[1], O_RDONLY);
-	readMe = read(from, buff, 1024);
+	readMe = read(from, buffer, 1024);
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
@@ -78,27 +78,27 @@ int main(int argc, char *argv[])
 		{
 			dprintf(STDERR_FILENO,
 				"Error: Can't read from file %s\n", argv[1]);
-			free(buff);
+			free(buffer);
 			exit(98);
 		}
 
-		writeMe = write(to, buff, readMe);
+		writeMe = write(to, buffer, readMe);
 		if (to == -1 || writeMe == -1)
 		{
 			dprintf(STDERR_FILENO,
 				"Error: Can't write to %s\n", argv[2]);
-			free(buff);
+			free(buffer);
 			exit(99);
 		}
 
-		readMe = read(from, buff, 1024);
+		readMe = read(from, buffer, 1024);
 		to = open(argv[2], O_WRONLY | O_APPEND);
 
 	} while (readMe > 0);
 
-	free(buff);
-	closeFile(from);
-	closeFile(to);
+	free(buffer);
+	close_file(from);
+	close_file(to);
 
 	return (0);
 }
